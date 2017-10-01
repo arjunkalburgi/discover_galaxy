00d20ea5edfc88e50ae6e989d79b9eb1442d55af
Arjun Kalburgi <askalburgi@gmail.com>
2017-10-01 12:54:41 -0700

    background and better graph moving

--- GraphApp.js
+++ GraphApp.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, View, Alert, Image } from 'react-native';
+const remote = 'https://upload.wikimedia.org/wikipedia/commons/a/a2/Polycyclic_Aromatic_Hydrocarbons_In_Space.jpg';
 
 export default class App extends React.Component {
 
@@ -11,6 +12,14 @@ export default class App extends React.Component {
 
 	componentWillMount() {
 		// console.warn(JSON.stringify(this.props.navigation.state.params.connections))
+		Alert.alert(
+			'onLongPress',
+			'Long press the nodes to see them in the chat',
+			[
+				{text: 'OK', onPress: () => {}},
+			],
+			{ cancelable: true }
+		)
 		this.setState({
 
 			parent: this.props.navigation.state.params.connections.parent,
@@ -21,19 +30,30 @@ export default class App extends React.Component {
 	}
 
 	async sendtoserver(text) {
-
 		this.props.navigation.state.params.buttonClicked(text); 
 		this.props.navigation.goBack()
 	}
 
+	async roamgraph(text) {
+		// const response = await fetch("http://localhost:8080/messages/" + encodeURI(text)); 
+		const response = await fetch("https://backgalaxy.crumb33.hasura-app.io/messages/" + encodeURI(text)); 
+		const data = await response.json(); 
+
+		this.setState({
+			parent: data.explore_terms.parent,
+			key: data.explore_terms.key,
+			graph: data.explore_terms.graph
+		});
+	}
+
 	nodefromparent(keytext) {
-		return (<TouchableOpacity style={[othernodes.wrapper, {top: -100}]} onPress={()=>{this.sendtoserver(keytext)}}>
+		return (<TouchableOpacity style={[othernodes.wrapper, {top: -100}]} onPress={()=>{this.roamgraph(keytext)}} onLongPress={()=>{this.sendtoserver(keytext)}}>
 					<Text style={{color: '#fff'}}>{keytext}</Text>
 				</TouchableOpacity>)
 	}
 
 	nodefromkey(keytext) {
-		return (<TouchableOpacity style={[currnode.wrapper]} onPress={()=>{this.sendtoserver(keytext)}}>
+		return (<TouchableOpacity style={[currnode.wrapper]} onPress={ () => this.sendtoserver(keytext) }>
 					<Text>{keytext}</Text>
 				</TouchableOpacity>)
 	}
@@ -46,7 +66,7 @@ export default class App extends React.Component {
 		// console.log(JSON.stringify(node))
 		return (<TouchableOpacity key={i} 
 									style={[othernodes.wrapper, {top: node.place.top, left: node.place.left}]} 
-									onPress={ () => this.sendtoserver(node.name) }>
+									onPress={()=>{this.roamgraph(node.name)}} onLongPress={()=>{this.sendtoserver(node.name)}}>
 					<Text style={{color: '#fff', textAlign: 'center'}}>{node.name}</Text>
 				</TouchableOpacity>)
 	}
@@ -61,19 +81,32 @@ export default class App extends React.Component {
 
 	render() {
 		return (
-			<View style={styles.container}>
-				{this.parent()}
-				{this.keynode()}
-				{this.children()}
-			</View>
+			<Image source={{ uri: remote }} style={backgroundImage.backgroundImage}>
+				<View style={styles.container}>
+					{this.parent()}
+					{this.keynode()}
+					{this.children()}
+				</View>
+			</Image>
+
+
 		)
 	}
 }
 
+const backgroundImage = StyleSheet.create({
+  	backgroundImage: {
+	    flex: 1,
+	    resizeMode: 'cover', // or 'stretch'
+	    width: '100%',
+	    height: '100%',
+	}
+})
+
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: 'transparent',
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
